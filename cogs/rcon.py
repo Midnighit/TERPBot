729f18a583f4d517afc701f85d5594b4b63ca02a
unknown <alexeuler.ffm@gmail.com>
2020-09-03 21:28:57 +0200

    added whitelistall command

--- cogs/rcon.py
+++ cogs/rcon.py
@@ -324,6 +324,56 @@ class RCon(commands.Cog, name="RCon commands"):
         print(f"Author: {ctx.author} / Command: {ctx.message.content}. {msg}")
         logger.info(f"Author: {ctx.author} / Command: {ctx.message.content}. {msg}")
 
+    @command(name='whitelistall', help="Whitelists everyone who's currently in the supplemental databas. Only works while server is down.")
+    @has_role_greater_or_equal(SUPPORT_ROLE)
+    async def whitelistall(self, ctx):
+        if RCon.is_running("ConanSandboxServer"):
+            await ctx.send("Command can only be used while server isn't running.")
+            logger.info(f"Author: {ctx.author} / Command: {ctx.message.content}.")
+            return
+        # try to read all entries already in the whitelist.txt file
+        try:
+            with open(WHITELIST_PATH, 'r') as f:
+                lines = f.readlines()
+        # if file doesn't exist create an empty list
+        except:
+            with open(WHITELIST_PATH, 'w') as f:
+                pass
+            lines = []
+
+        # split lines into id and name. Remove duplicates.
+        filtered = set()
+        names = {}
+        for line in lines:
+            if line != "\n" and not "INVALID" in line:
+                res = line.split(':')
+                id = res[0].strip()
+                if len(res) > 1:
+                    name = res[1].strip()
+                else:
+                    name = 'Unknown'
+                filtered.add(id)
+                # if duplicate values exist, prioritize those containing a funcom_name
+                if not id in names or names[id] == 'Unknown':
+                    names[id] = name
+
+        # go through the Users table and supplement missing users if any
+        for user in session.query(Users).all():
+            if not user.funcom_id in filtered:
+                filtered.add(user.funcom_id)
+                names[user.funcom_id] = 'Unknown'
+
+        # create lines to write into new whitelist.txt
+        wlist = []
+        for id in filtered:
+            wlist.append(id + ':' + names[id] + '\n')
+            wlist.sort()
+
+        # overwrite / write the new file with the contenst of wlist
+        with open(WHITELIST_PATH, 'w') as f:
+            f.writelines(wlist)
+        await ctx.send("All players in the supplemental database have been placed on the whitelist.")
+
     @command(name='gettime', help="Tells the current time on the server")
     async def gettime(self, ctx):
         try:
